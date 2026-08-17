021d0e7fe35f0a1cd71f50db4039e8a2e1643896
Nicole Cayo <nickybelu@gmail.com>
2026-08-16 22:39:59 -0300

    first commit

---
+++ src/main/java/com/nkydev/SoftwareEngineer.java
@@ -0,0 +1,55 @@
+package com.nkydev;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private List<String> techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(int id, String name, List<String> techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(List<String> techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
